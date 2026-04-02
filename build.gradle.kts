5fc4a7f1ac6b2f961dbdcbefff1b89cffad1de97
Maksym Zapisov <max.zapisov@gmail.com>
2026-04-03 01:18:32 +0200

    test: Added E2E test and cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ val restAssuredVersion = "6.0.0"
 val jacksonDatabindVersion = "2.21.1"
 val assertJVersion = "3.27.7"
 val junitVersion = "6.0.3"
+val commonsValidatorVersion = "1.10.1"
 val ownerVersion = "1.0.12"
 val allureVersion = "2.33.0"
 val commonsCodecVersion = "1.21.0"
@@ -32,6 +33,7 @@ dependencies {
     implementation("io.rest-assured:rest-assured:$restAssuredVersion")
     implementation("io.rest-assured:json-schema-validator:$restAssuredVersion")
     implementation("org.aeonbits.owner:owner:${ownerVersion}")
+    testImplementation("commons-validator:commons-validator:$commonsValidatorVersion")
     testImplementation("org.assertj:assertj-core:$assertJVersion")
     testImplementation("org.slf4j:slf4j-simple:${slf4jVersion}")
     testImplementation(platform("io.qameta.allure:allure-bom:$allureVersion"))
@@ -41,6 +43,42 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+    reports {
+        junitXml.required.set(true)
+        html.required.set(true)
+        junitXml.outputLocation.set(layout.projectDirectory.dir("test-results/junit/$name/xml"))
+        html.outputLocation.set(layout.projectDirectory.dir("test-results/junit/$name/html"))
+    }
+}
+
+tasks.register<Test>("contractTest") {
+    group = "verification"
+    description = "Runs tests tagged with Contract"
+    testClassesDirs = sourceSets.test.get().output.classesDirs
+    classpath = sourceSets.test.get().runtimeClasspath
+    useJUnitPlatform {
+        includeTags("Contract")
+    }
+}
+
+tasks.register<Test>("functionalTest") {
+    group = "verification"
+    description = "Runs tests tagged with Functional"
+    testClassesDirs = sourceSets.test.get().output.classesDirs
+    classpath = sourceSets.test.get().runtimeClasspath
+    useJUnitPlatform {
+        includeTags("Functional")
+    }
+}
+
+tasks.register<Test>("e2eTest") {
+    group = "verification"
+    description = "Runs tests tagged with E2E"
+    testClassesDirs = sourceSets.test.get().output.classesDirs
+    classpath = sourceSets.test.get().runtimeClasspath
+    useJUnitPlatform {
+        includeTags("E2E")
+    }
 }
---
+++ src/test/java/dev/maksymzapisov/jsonplaceholder/e2e/SearchUserGetPostsAndValidateCommentsTest.java
@@ -0,0 +1,79 @@
+package dev.maksymzapisov.jsonplaceholder.e2e;
+
+import dev.maksymzapisov.jsonplaceholder.BaseApiTest;
+import dev.maksymzapisov.jsonplaceholder.clients.PostsClient;
+import dev.maksymzapisov.jsonplaceholder.clients.UsersClient;
+import dev.maksymzapisov.jsonplaceholder.data.TestDataProvider;
+import dev.maksymzapisov.jsonplaceholder.model.Comment;
+import dev.maksymzapisov.jsonplaceholder.model.Post;
+import dev.maksymzapisov.jsonplaceholder.model.User;
+import dev.maksymzapisov.jsonplaceholder.specs.DefaultJsonSpecs;
+import dev.maksymzapisov.jsonplaceholder.utils.ResponseExtractors;
+import org.apache.commons.validator.routines.EmailValidator;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SearchUserGetPostsAndValidateCommentsTest extends BaseApiTest {
+
+    private final UsersClient userClient = new UsersClient();
+    private final PostsClient postClient = new PostsClient();
+
+    @Tag("E2E")
+    @Test
+    @DisplayName("Search user, get their posts and validate email in comments for each post")
+    public void searchUserGetPostsAndValidateComments() {
+        User existingUser = TestDataProvider.getExistingUser();
+
+        Response usersResponse = userClient.getUsersByQueryParams(Map.of("username", existingUser.getUsername()));
+
+        usersResponse.then()
+                    .spec(DefaultJsonSpecs.defaultJsonResponseSpec())
+                    .extract();
+
+        List<User> foundUsers = ResponseExtractors.asList(usersResponse, User.class);
+
+        assertThat(foundUsers)
+                .isNotEmpty()
+                .hasSize(1)
+                .contains(existingUser);
+
+        User foundUser = foundUsers.getFirst();
+
+        Response postsResponse = postClient.getPostsByQueryParams(Map.of("userId", String.valueOf(foundUser.getId())));
+
+        postsResponse.then()
+                    .spec(DefaultJsonSpecs.defaultJsonResponseSpec())
+                    .extract();
+
+        List<Post> foundUserPosts = ResponseExtractors.asList(postsResponse, Post.class);
+
+        assertThat(foundUserPosts)
+                .isNotEmpty()
+                .hasSizeGreaterThanOrEqualTo(1)
+                .allMatch(post -> post.getUserId() == foundUser.getId());
+
+
+        foundUserPosts.forEach(post -> {
+            Response commentsResponse = postClient.getCommentsForPost(post.getId());
+
+            commentsResponse.then()
+                        .spec(DefaultJsonSpecs.defaultJsonResponseSpec())
+                        .extract();
+
+            List<Comment> foundCommentsForPost = ResponseExtractors.asList(commentsResponse, Comment.class);
+
+            assertThat(foundCommentsForPost)
+                    .isNotEmpty()
+                    .hasSizeGreaterThanOrEqualTo(1)
+                    .allMatch(comment -> comment.getPostId() == post.getId())
+                    .allMatch(comment -> EmailValidator.getInstance().isValid(comment.getEmail()));
+        });
+    }
+}
